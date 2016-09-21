fd4366fce0112b78b440f0e557f0e80aa1cce921
Jeremy Magland <jeremy.magland@gmail.com>
2016-09-21 09:52:35 -0400

    remove

--- src/prv.h
+++
@@ -1,9 +0,0 @@
-/******************************************************
-** See the accompanying README and LICENSE files
-** Author(s): Jeremy Magland
-** Created: 9/21/2016
-*******************************************************/
-#ifndef PRV_H
-#define PRV_H
-
-#endif // PRV_H
